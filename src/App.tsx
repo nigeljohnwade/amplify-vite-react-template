7f3d31796509cfe6913887f88b7ed99d549e4768
Nigel Wade <Nigel.Wade@cyviz.com>
2025-11-08 07:27:39 +0000

    Add user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { useEffect, useState } from 'react';
+import type { Schema } from '../amplify/data/resource';
+import { generateClient } from 'aws-amplify/data';
+import { useAuthenticator} from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const [todos, setTodos] = useState<Array<Schema['Todo']['type']>>([]);
+    const { user, signOut } = useAuthenticator();
+    useEffect(() => {
+        client.models.Todo.observeQuery().subscribe({
+            next: (data) => setTodos([...data.items]),
+        });
+    }, []);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+    function createTodo() {
+        client.models.Todo.create({content: window.prompt('Todo content')});
+    }
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+    function deleteTodo(id: string) {
+        client.models.Todo.delete({id});
+    }
 
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <h1>{user?.signInDetails?.loginId}'s todos <button onClick={signOut}>Sign out</button></h1>
+            <button onClick={createTodo}>+ new</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.content} <button onClick={() => deleteTodo(todo.id)}>Delete</button></li>
+                ))}
+            </ul>
+        </main>
+    );
 }
 
 export default App;
