33951d724eab58d8c79bd780f181ac45ab42340c
Nigel Wade <Nigel.Wade@cyviz.com>
2026-06-18 09:02:29 +0100

    Remove category

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,10 +9,6 @@ key can "create", "read","update", and "delete" any "Todo" records.
 This has been changed to restrict actions to the owner.
 =========================================================================*/
 const schema = a.schema({
-    Category: a.model({
-        value: a.string(),
-        displayName: a.string(),
-    }),
     Location: a.customType({
         lat: a.float(),
         long: a.float(),
