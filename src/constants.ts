1511fabaa3f5b2861640b1ed93f39e7a4f6457c9
Nigel Wade <Nigel.Wade@cyviz.com>
2026-06-18 09:59:54 +0100

    Remove unused components

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const INITIAL_CENTER: [number, number] = [-3.18393, 55.96118];
-export const INITIAL_ZOOM = 15;
-
-export const categories = [
-    {id: 0, value: 'home', displayName: 'Home'},
-    {id: 1, value: 'work', displayName: 'Work'},
-];
