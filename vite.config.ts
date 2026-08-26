e74039341131ed8451543d6544178f191bcf40cc
Nigel Wade <Nigel.Wade@cyviz.com>
2026-08-26 08:34:27 +0100

    Adjust chunk warning size

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        chunkSizeWarningLimit: 3000,
+    },
     plugins: [
         react(),
         tsConfigPaths(),
